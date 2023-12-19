27f14440b01c324c33ffcae5c2e5b6775c88ea03
indhayare1 <109117000+indhayare1@users.noreply.github.com>
2023-12-19 13:14:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+imp1=float(input("hours of Employees Worked : "))
+
+if imp1 > 40 :
+    imp2= imp1 * 15
+    print("Well Done ✅",imp2)
+  
+if imp1 == 40:
+   imp3=imp1*12
+   print("Excellent Job ✨ Let's Continue🤧",imp3)
+    
+if imp1 < 40: 
+      imp4 = imp1*10
+      print("improve The time",imp4)
+
+print("Thank you!!")
+
+# tihaabi samee
+#1 kuqor number 39 <
+#2 kuqor number 40 ==
+#3 kuqor number 41 >
+#I'm going to add Promo Code Option
